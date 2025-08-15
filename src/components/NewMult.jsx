a0fd00e93b9e2bdfab457f5eea49231eb5ff4320
sheyh77 <ubabaniyozov@gmail.com>
2025-08-15 22:05:01 +0500

    xato zotildi

--- src/components/NewMult.jsx
+++ src/components/NewMult.jsx
@@ -14,10 +14,6 @@ function NewMult() {
             <div className="cantainer">
                 <div className="newMult-wrap">
                     <h1 className='newMult-title'>Yangi multfilmlar</h1>
-                    <img src="/images/newMult-decor1.png" alt="tom" className='newMult-decor1 newMult-decor' />
-                    <img src="/images/newMult-decor2.png" alt="tom" className='newMult-decor2 newMult-decor' />
-                    <img src="/images/newMult-decor3.png" alt="jerry" className='newMult-decor3 newMult-decor' />
-                    <img src="/images/newMult-decor4.png" alt="tom" className='newMult-decor4 newMult-decor' />
                     <Swiper navigation={true} modules={[Navigation]} className="newMult-swiper mySwiper">
                         {
                             newMultMap.map((item) => (
