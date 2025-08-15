89b862f94e8e39fa797ba9e783643ad183fb6493
sheyh77 <ubabaniyozov@gmail.com>
2025-08-16 02:23:13 +0500

    mulfilm page

---
+++ src/data/multMap.js
@@ -0,0 +1,122 @@
+export const multMap = [
+    {
+        id: 1,
+        img: "/images/mult-img1.jpg",
+        title: "Dovyurak oshpaz",
+        lang: "uz"
+    },
+    {
+        id: 2,
+        img: "/images/mult-img2.jpg",
+        title: "Oltin Tarvuz",
+        lang: "uz"
+    },
+    {
+        id: 3,
+        img: "/images/mult-img3.jpg",
+        title: "Zumrad va Qimmat",
+        lang: "uz"
+    },
+    {
+        id: 4,
+        img: "/images/mult-img4.jpg",
+        title: "Pinokio",
+        lang: "ru"
+    },
+    {
+        id: 5,
+        img: "/images/mult-img5.jpeg",
+        title: "Tepaga",
+        lang: "uz"
+    },
+    {
+        id: 6,
+        img: "/images/mult-img6.jpg",
+        title: "Kichkina xo'jayin",
+        lang: "uz"
+    },
+    {
+        id: 7,
+        img: "/images/mult-img7.jpg",
+        title: "Kungfu Panda",
+        lang: "uz"
+    },
+    {
+        id: 8,
+        img: "/images/mult-img8.jpg",
+        title: "Asalari Maya",
+        lang: "uz"
+    },
+    {
+        id: 9,
+        img: "/images/mult-img9.jpg",
+        title: "Do'ppi Pomidor",
+        lang: "uz"
+    },
+    {
+        id: 10,
+        img: "/images/mult-img10.jpg",
+        title: "Ayiqvoy va Masha",
+        lang: "uz"
+    },
+    {
+        id: 11,
+        img: "/images/newMult-peppa1.jpg",
+        title: "Peppa pig",
+        lang: "en"
+    },
+    {
+        id: 12,
+        img: "/images/newMult-pocoyo.jpg",
+        title: "Pocoyo",
+        lang: "en"
+    },
+    {
+        id: 13,
+        img: "/images/newMult-postman.webp",
+        title: "Postman Pet",
+        lang: "en"
+    },
+    {
+        id: 14,
+        img: "/images/newMult-smeshariki.jpg",
+        title: "Smeshariki",
+        lang: "ru"
+    },
+    {
+        id: 15,
+        img: "/images/newMult-oybola.webp",
+        title: "Oybola",
+        lang: "ru"
+    },
+    {
+        id: 16,
+        img: "/images/newMult-img9.jpg",
+        title: "Nu pogodi",
+        lang: "ru"
+    },
+    {
+        id: 17,
+        img: "/images/newMult-img7.webp",
+        title: "Chip va Deyl",
+        lang: "ru"
+    },
+    {
+        id: 18,
+        img: "/images/newMult-img6.png",
+        title: "Murvatchalar",
+        lang: "uz"
+    },
+    {
+        id: 19,
+        img: "/images/newMult-img5.webp",
+        title: "Tom va Jerry",
+        lang: "en"
+    },
+    {
+        id: 20,
+        img: "/images/newMult-img4.webp",
+        title: "Uch bahodir",
+        lang: "uz"
+    }
+]
--- src/pages/Mult.jsx
+++ src/pages/Mult.jsx
@@ -1,9 +1,168 @@
-import React from 'react'
+// import React, { useRef, useState } from 'react';
+// import Header from "../layout/Header";
+// import { newMultMap } from "../components/newMultMap";
+
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+// import { Autoplay, Pagination } from 'swiper/modules';
+// import { Link } from 'react-router-dom';
+// import { multMap } from '../data/multMap';
+
+// function Mult() {
+//   return (
+//     <section className='mult'>
+//       <div className="cantainer">
+//         <div className="mult-wrap">
+//           <Header />
+//           <input type="search"
+//             placeholder='Qidiruv'
+//             className='mult-search'
+//           />
+//           <div className="mult-top">
+//             <Swiper
+//               spaceBetween={30}
+//               centeredSlides={true}
+//               autoplay={{
+//                 delay: 2500,
+//                 disableOnInteraction: false,
+//               }}
+//               pagination={{
+//                 clickable: true,
+//               }}
+//               modules={[Autoplay, Pagination]}
+//               className="mySwiper"
+//             >
+//               {
+//                 newMultMap.map((item) => (
+//                   <SwiperSlide key={item.id}>
+//                     <img src={item.img} alt="item image" className='newMult-swiper-img' />
+//                   </SwiperSlide>
+//                 ))
+//               }
+//             </Swiper>
+//           </div>
+//           <div className="mult-card">
+//             <div className="mult-card-option">
+//               <button>Uzbek</button>
+//               <button>English</button>
+//               <button>Russion</button>
+//             </div>
+//             <div className="mult-card-cards">
+//               {
+//                 multMap.map((item) => (
+//                   <div className="mult-cards-item" key={item.id}>
+//                     <Link>
+//                       <img src={item.img} alt="mult" />
+//                       <p className="mult-cards-title">{item.title}</p>
+//                     </Link>
+//                   </div>
+//                 ))
+//               }
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   )
+// }
+
+// export default Mult
+
+import React, { useState } from 'react';
+import Header from "../layout/Header";
+import { newMultMap } from "../components/newMultMap";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination } from 'swiper/modules';
+import { Link } from 'react-router-dom';
+import { multMap } from '../data/multMap';
 
 function Mult() {
+  const [selectedLang, setSelectedLang] = useState("uz");
+
+  // Tanlangan tilga qarab filtrlash
+  const filteredMults = multMap.filter(item => item.lang === selectedLang);
+
   return (
-    <div>Mult</div>
-  )
+    <section className='mult'>
+      <div className="cantainer">
+        <div className="mult-wrap">
+          <Header />
+
+          {/* Qidiruv */}
+          <input
+            type="search"
+            placeholder='Qidiruv'
+            className='mult-search'
+          />
+
+          {/* Swiper top qismi */}
+          <div className="mult-top">
+            <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Autoplay, Pagination]}
+              className="mySwiper"
+            >
+              {newMultMap.map((item) => (
+                <SwiperSlide key={item.id}>
+                  <img src={item.img} alt="item image" className='newMult-swiper-img' />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
+          {/* Cardlar bo‘limi */}
+          <div className="mult-card">
+            {/* Til tanlash tugmalari */}
+            <div className="mult-card-option">
+              <button
+                className={selectedLang === "uz" ? "active" : ""}
+                onClick={() => setSelectedLang("uz")}
+              >
+                Uzbek
+              </button>
+              <button
+                className={selectedLang === "en" ? "active" : ""}
+                onClick={() => setSelectedLang("en")}
+              >
+                English
+              </button>
+              <button
+                className={selectedLang === "ru" ? "active" : ""}
+                onClick={() => setSelectedLang("ru")}
+              >
+                Russion
+              </button>
+            </div>
+
+            {/* Filtrlangan cardlar */}
+            <div className="mult-card-cards">
+              {filteredMults.map((item) => (
+                <div className="mult-cards-item" key={item.id}>
+                  <Link to={item.to}>
+                    <img src={item.img} alt="mult" />
+                    <p className="mult-cards-title">{item.title}</p>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Mult
+export default Mult;
